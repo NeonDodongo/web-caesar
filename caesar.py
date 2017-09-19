b2908b099713078d9756c11b7dfa7858e78cf76c
neondodongo <willirmler@me.com>
2017-09-19 16:52:19 -0500

    Final web-caesar assignment

--- caesar.py
+++ caesar.py
@@ -10,8 +10,11 @@ def alphabet_position(letter):
         return low_alpha.find(letter)
 
 def rotate_character(char, rot):
-    original_pos = alphabet_position(char)
-    new_pos = original_pos + rot
+    if char.isalpha():
+        original_pos = alphabet_position(char)
+        new_pos = original_pos + rot
+    else:
+        return char
 
     if new_pos >= 26:
         new_pos = new_pos % 26
@@ -20,8 +23,6 @@ def rotate_character(char, rot):
         return string.ascii_lowercase[new_pos]
     elif char in string.ascii_uppercase:
         return string.ascii_uppercase[new_pos]
-    else:
-        return char
 
 def encrypt_string(text, rot):
     new_text = ""
